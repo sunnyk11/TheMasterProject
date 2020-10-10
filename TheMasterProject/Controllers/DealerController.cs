a4ac8f6e0fc4327cce02ee1bb45d9269165f02e5
sunny kumar <sunny971613@gmail.com>
2020-10-10 09:52:51 +0530

    Updation related to Master Project

---
+++ TheMasterProject/Controllers/DealerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheMasterProject.Controllers
+{
+    public class DealerController : Controller
+    {
+        public string GetUserId()
+        {
+            var userId = Session["UserId"].ToString();
+            return userId;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
